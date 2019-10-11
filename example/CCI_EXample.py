b873e28046fc7812c1056a852bc3084858ba202f
yutiansut <yutiansut@qq.com>
2019-10-12 03:08:32 +0800

    #

---
+++ example/CCI_EXample.py
@@ -0,0 +1,41 @@
+from QAStrategy import QAStrategyCTABase
+import QUANTAXIS as QA
+import pprint
+
+
+class CCI(QAStrategyCTABase):
+
+    def on_bar(self, bar):
+
+        res = self.cci()
+
+        print(res.iloc[-1])
+
+        if res.CCI[-1] < -100:
+
+            print('LONG')
+
+            if self.positions.volume_long == 0:
+                self.send_order('BUY', 'OPEN', price=bar['close'], volume=1)
+
+            if self.positions.volume_short > 0:
+                self.send_order('SELL', 'CLOSE', price=bar['close'], volume=1)
+
+        elif res.CCI[-1] > 100:
+            print('SHORT')
+            if self.positions.volume_short == 0:
+                self.send_order('SELL', 'OPEN', price=bar['close'], volume=1)
+            if self.positions.volume_long > 0:
+                self.send_order('BUY', 'CLOSE', price=bar['close'], volume=1)
+
+    def cci(self,):
+        return QA.QA_indicator_CCI(self.market_data, 61)
+
+    def risk_check(self):
+        pass
+        # pprint.pprint(self.qifiacc.message)
+
+
+if __name__ == '__main__':
+    CCI(code='RB2001', frequence='1min',
+        strategy_id='1dd8b22d-7902-4a85-adb2-fbac4bb977fe').run()
