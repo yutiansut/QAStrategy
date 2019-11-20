ab123c1211f155e28efb039258b3a72b26cd5432
yutiansut <yutiansut@qq.com>
2019-11-20 09:29:54 +0800

    fixbug

--- QAStrategy/qactabase.py
+++ QAStrategy/qactabase.py
@@ -283,8 +283,7 @@ class QAStrategyCTABase():
             
             import re
             self._num_cached  = 2*int(re.findall(r'\d+',self.frequence)[0])
-            self.sub = subscriber(exchange='realtime_min_{}'.format(
-                code), host=data_host, port=data_port, user=data_user, password=data_password)
+            self.sub = subscriber_routing(exchange='CTPX', routing_key=code, host=data_host, port=data_port, user=data_user, password=data_password)
             self.sub.callback = self.second_callback
         elif frequence.endswith('tick'):
             self._num_cached = 1
